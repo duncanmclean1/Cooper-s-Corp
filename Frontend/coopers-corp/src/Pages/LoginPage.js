fa71133fa7954d0963198f977ee219c826c415f6
Patel <sahil.patel@fiserv.com>
2023-11-16 16:11:45 -0500

    Implementation of login page.

--- Frontend/coopers-corp/src/Pages/LoginPage.js
+++ Frontend/coopers-corp/src/Pages/LoginPage.js
@@ -1,18 +1,29 @@
-import {Container, Box, Typography, TextField, FormControlLabel, Checkbox, Button, Grid, Link} from "@material-ui/core";
-import { useEffect, useState } from "react";
+import {Container, Box, Typography, TextField, Button, Grid, Link} from "@material-ui/core";
+import { useState } from "react";
 export default function Login() {
-      const [employees, setEmployees] = useState([])
+      const [employeeId, setEmployeeId] = useState("");
+      const [password, setPassword] = useState("");
 
+      const validateForm = () =>
+      {
+        return employeeId > 0 && password.length > 0;
+      }
+      
       const handleSubmit = (event) => {
         event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-          email: data.get("email"),
-          password: data.get("password"),
-        });
+        const example = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json'},
+          body: JSON.stringify({title: 1})
+        };
+        fetch('/api/login', example)
+        .then(response => {
+            response.json()
+        })
+        .then(data => {
+          console.log(data)
+        })
       };
- 
-
       return (
         <Container component="main" maxWidth="xs">
           <Box
@@ -31,11 +42,13 @@ export default function Login() {
                 margin="normal"
                 required
                 fullWidth
-                id="email"
+                id="employee id"
                 label="Employee Id"
-                name="email"
-                autoComplete="email"
+                name="employee id"
+                autoComplete="employee id"
                 autoFocus
+                value={employeeId}
+                onChange={event => setEmployeeId(event.target.value)}
               />
               <TextField
                 margin="normal"
@@ -46,16 +59,16 @@ export default function Login() {
                 type="password"
                 id="password"
                 autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
+                value={password}
+                onChange={event => setPassword(event.target.value)}
               />
               <Button
                 type="submit"
                 fullWidth
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
+                onSubmit={handleSubmit}
+                disabled={!validateForm()}
               >
                 Sign In
               </Button>
